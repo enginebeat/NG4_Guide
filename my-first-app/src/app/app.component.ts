7cc6e871c367f2d2916ae362e3eb90ec5c5db872
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-05-24 08:40:15 +0100

    using styleUrls or inline styles

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //styleUrls: ['./app.component.css'] // not that this is an array as multiple css templates can be referenced
+  // using inline styling. note that you can only have one or the other
+  styles:[`
+    h3{
+      color: dodgerblue;
+    }
+  `]
+
 })
 export class AppComponent {
   title = ' my app';
