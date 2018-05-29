97959366410cce2004f5f18530deda7e32906284
DESKTOP-0E86V1G\luism <luis.moreira@lmtronics.co.uk>
2018-05-29 18:42:47 +0100

    databinding. String interlpolation, property binding, event binding & two way databinding

---
+++ basic-modules/src/app/servers/servers.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: 'app-servers',
+    templateUrl: './servers.component.html',
+    styleUrls: ['./servers.component.css']
+})
+
+export class ServersComponent implements OnInit { 
+    allowNewServer = false;
+    serverCreationStatus="No server was created!";
+    hoveringButton = 'You are not on top of the button';
+    serverName = 'TestServer';
+
+    constructor(){
+        setTimeout(()=>{
+            this.allowNewServer = true;
+        }, 2000);
+    }
+
+    ngOnInit(){
+
+    }
+    
+    onCreateServer(){
+        this.serverCreationStatus = `${this.serverName} Server was created!`;
+    }
+
+    onHoverButton(){
+        this.hoveringButton = 'You are on top of the button';
+    }
+
+    onUpdateServerName(event: any){
+        console.log(event.target.value);
+        this.serverName = (<HTMLInputElement>event.target).value;
+    }
+}
