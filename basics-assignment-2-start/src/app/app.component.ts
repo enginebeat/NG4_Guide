e7a76e1bc97ebbd4696e08b47d6de2cebf2aab9c
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-05-31 08:13:22 +0100

    finally done it! not quite sure what happened, I tried both solutions before... never mind.

--- basics-assignment-2-start/src/app/app.component.ts
+++ basics-assignment-2-start/src/app/app.component.ts
@@ -6,11 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  username = "";
-  clearValue(){
-    input_username
-  }
-
+  username="";
   
+  resetUser(){
+    this.username = "";
+  }
   
 }
