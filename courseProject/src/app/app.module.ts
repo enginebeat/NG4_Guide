b54c8c92320250e34089737f4214748a8ef74870
Moreira, Luis A <luis.moreira@ukaea.uk>
2018-06-04 15:12:51 +0100

    first commit of the courseProject

---
+++ courseProject/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './header/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './header/shoppingList/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './header/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './header/recipeList/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './header/recipeList/recipe-detail/recipe-detail.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
